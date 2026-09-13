9a13d4378a61675c48cb9c4f534658da40d85165
Huy Le Quang <91176131+quagh03@users.noreply.github.com>
2026-09-13 08:32:07 +0700

    backend initial commit

---
+++ backend/src/test/java/com/huylq/it6027/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huylq.it6027.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
